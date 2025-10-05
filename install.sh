78ded3c4f21c2bf2c5d30b8fefb325d17e6940e9
MeexReay <meexreay@gmail.com>
2025-10-06 00:51:03 +0300

    install: new install script omg!!!

--- install.sh
+++ install.sh
@@ -1,123 +1,53 @@
-for arg in "$@"; do
-  case "$arg" in
-    -rm)
-	    echo "[envfetch installer] do you want to delete everything in this directory? (y/n): "
-	    read -r wow
-	    if [ "$wow" = "y" ]; then
-		echo "[envfetch installer] cleaning directory..."
-	    	rm -rvf ./*
-		echo "[envfetch installer] removed"
-		exit 0
-	    elif [ "$wow" = "n" ]; then
-		echo "[envfetch installer] closing installer..."
-		exit 0
-	    else
-		echo "[envfetch installer] not an y/n: closing installer..."
-		exit 1
-	    fi
-      ;;
-    -rmbin)
-	    echo "[envfetch installer] do you want to delete installed envfetch? (y/n): "
-	    read -r wow2
-	    if [ "$wow2" = "y" ]; then
-		echo "[envfetch installer] removing envfetch..."
-                rm -rvf /bin/envfetch.d
-		echo "[envfetch installer] removed"
-		exit 0
-	    elif [ "$wow2" = "n" ]; then
-		echo "[envfetch installer] closing installer..."
-		exit 0
-	    else
-		echo "[envfetch installer] not an y/n: closing installer..."
-		exit 1
-	    fi
-      ;;
-    -install)
-	    echo "[envfetch installer] preparing for the installation..."
-		echo "[envfetch installer] shell is $(ps -p $$ -o comm=)"
-	    if [ -d /bin/envfetch.d ]; then
-		    echo "preparing: failed: already installed"
-		    exit 1
-	    else
-		    echo "preparing: 30%"
-	    fi
-	    if [ -f main.sh ]; then
-		    echo "preparing: 60%"
-	    else
-		    echo "preparing: failed: no main.sh in this directory"
-	    fi
-	    if [ -d ascii ]; then
-		   echo "preparing: 100%"
-	    else
-		   echo "preparing: failed: no ascii directory"
-	    fi
-	    echo "[envfetch installer] prepared successfully"
-	    echo "[envfetch installer] do you want to install envfetch? (y/n): "
-	    read -r wow3
-	    if [ "$wow3" = "y" ]; then
-		    echo "[envfetch installer] proceeding with installing"
-	    elif [ "$wow3" = "n" ]; then
-		    echo "[envfetch installer] stopping the installing process..."
-		    exit 0
-	    else
-		    echo "[envfetch installer] not an y/n: stopping the installing process..."
-		    exit 1
-	    fi
-	    echo "[envfetch installer] checking for the root rights..."
-	    if [ "$(id -u)" != 0 ]; then 
-		    echo "[envfetch installer] start from the root user!"
-		    exit 1
-	    fi
-	    echo "[envfetch installer] checked successfully"
-	    echo "[envfetch installer] checking for the /bin directory..."
-	    if [ -d /bin ]; then
-		    echo "[envfetch installer] checked successfully"
-	    else
-		    echo "[envfetch installer] not FHS system/doesn't have /bin directory!"
-		    exit 1
-	    fi
-	    echo "[envfetch installer] checked successfully!"
-	    echo "[envfetch installer] making the /bin/envfetch.d directory..."
-	    mkdir /bin/envfetch.d || {
-	    	echo "[envfetch installer] error with making /bin/envfetch.d directory! aborting the installing process..."
-	    	exit 1
-	    }
-	    echo "[envfetch installer] maked successfully!"
-	    echo "[envfetch installer] copying the main.sh file to /bin/envfetch.d"
-	    cp main.sh /bin/envfetch.d/main.sh || {
-	    	echo "[envfetch installer] error with copying main.sh to /bin/envfetch.d directory! aborting the installing process..."
-	    exit 1
-	    }
-	    echo "[envfetch installer] successfully copied main.sh -> /bin/envfetch.d!"
-	    cp -r ascii /bin/envfetch.d/ascii || {
-	    	echo "[envfetch installer] error with copying ascii to /bin/envfetch.d directory! aborting the installing process..."
-	    exit 1
-	    }
-	    echo "[envfetch installer] successfully copied ascii -> /bin/envfetch.d!"
-	    echo "[envfetch installer] successfully installed!"
-	    if [ "$(ps -p $$ -o comm=)" = "bash" ]; then
-		    echo "[envfetch installer] adding an alias to ~/.bashrc (for envfetch)"
-		    echo "[envfetch installer] checking for ~/.bashrc..."
-		    if [ -f ~/.bashrc ]; then
-			echo "alias envfetch='cd /bin/envfetch.d;./main.sh;cd - > /dev/null'" >> /home/$SUDO_USER/.bashrc  # alias from meexreay thanks
-			echo "[envfetch installer] added an alias into ~/.bashrc successfully!"
-		    else
-			echo "[envfetch installer] you use BASH but doesn't have ~/.bashrc: alias isn't added"
-		    fi
-	    else
-                    echo "[envfetch installer] you don't use BASH: add string alias envfetch='cd /bin/envfetch.d;./main.sh;cd - > /dev/null' into your shell config yourself!"
-	    fi
-	    echo ""
-	    echo "[envfetch installer] installed envfetch successfully!"
-	    exit 0
-	;;
-  esac
-done
+tesl() {
+  echo "[envfetch installer] $@"
+}
 
-echo "[envfetch installer]"
+SUDO=
+if [ "$EUID" = 0 ]; then SUDO=""
+elif command -v sudo > /dev/null; then SUDO="sudo ";
+elif command -v doas > /dev/null; then SUDO="doas ";
+elif command -v pkexec > /dev/null; then SUDO="pkexec ";
+else
+  tesl "NO ROOT/SUDO COMMAND!!!!"
+  tesl "f*ck! BROJEN ENV"
+  tesl
+  tesl "   _____ "
+  tesl "  < die >"
+  tesl "   ----- "
+  tesl "          \\   ^__^"
+  tesl "           \\  (oo)\\_______"
+  tesl "              (__)\\       )\\/\\"
+  tesl "                  ||----w |"
+  tesl "                  ||     ||"
+  tesl
+  tesl "run as root to install or if you are run with EUID=0"
+  tesl "your env: no sudo/doas/pkexec dude thats crazzy"
+  exit 1
+fi
+
+case "$1" in
+  install)
+  	tesl "install envspygornemt.."
+
+    $SUDO install -D ascii/* -t /usr/lib/envfetch/ascii
+    $SUDO install -m777 main.sh /usr/lib/envfetch/envfetch
+    $SUDO ln -s /usr/lib/envfetch/envfetch /usr/bin/envfetch
+     	
+  	exit 0
+	;;
+  uninstall)
+  	tesl "uninstall envspygornemt.."
+
+    $SUDO rm -r /usr/lib/envfetch /usr/bin/envfetch
+  	
+  	exit 0
+  ;;
+esac
+
+tesl
 echo "welcome to the installer"
-echo "if you want to install envfetch:       ./install.sh -install"
-echo "if you want to remove envfetch:        ./install.sh -rmbin"
-echo "if you want to clean this directory:   ./install.sh -rm"
+echo "if you want to install envfetch:       ./install.sh install"
+echo "if you want to remove envfetch:        ./install.sh uninstall"
 echo ""
-echo "envfetch installer v1.4 | locomia (c) 2025 | https://github.com/locomiadev"
+echo "envfetch installer v2.0 | locomia (c) 2025 | https://github.com/locomiadev"
+exit 1
