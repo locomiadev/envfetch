ba6d57875f65459255e7e8f042f71e6658d0b122
locomiadev <stikmenamerica@gmail.com>
2026-03-30 13:23:20 -0400

    +etc/ directory, +envfetch-v9 (Plan9 Port)

---
+++ etc/README.txt
@@ -0,0 +1,7 @@
+envfetch/etc
+- Folder for unnecessary files like Nix Flake files, Plan9-compat, Windows-compat (in future)
+- You can freely remove this if you don't have:
+
+- Plan9 / 9Front / 9ants / 9legacy & other 9 Family
+- NixOS (if you want to make package)
+- Windows (W.I.P)
---
+++ etc/plan9.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# envfetch v9 (Port for Plan9 systems)
+# Run with "APE sh" (ape/sh) !
+
+# Global variables
+ENVFETCH_VER="v9"
+
+# Main script
+if grep -s 'vmx' /dev/drivers; then
+	TOTAL=$(sed 1q /dev/swap | awk '{print int($1 / 1024 / 1024)}')
+	USED=$(grep user /dev/swap | awk -F/ '{print $1 * 4096 / 1024 / 1024}')
+	DE=$([ -e /dev/wsys ] && echo "rio" || echo "tty")
+	echo
+	echo "  ,-----,  $user@$sysname"
+	echo " |   _   | os:       9front "
+	echo " |  |_|  | ram:      $USED / $TOTAL MiB"
+	echo " |  ._|  | cpu:      $(cat '#P/cputype')"
+	echo "  '_____'  init:     9front init"
+	echo "           shell:    $0"
+	echo "           de/wm:    $DE"
+	echo "           envfetch: $ENVFETCH_VER"
+else
+	echo "You have an unsupported Plan9-based system. Please make an issue here: http://github.com/locomiadev/envfetch"
+fi
+
